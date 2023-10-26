2cd20e1c6f843ec7bc13278c8e732d17b337cd9a
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-26 01:11:57 -0300

    feat: store clients + message between clients + count connected clients + close connection

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"nhooyr.io/websocket"
 )
+
+var clients map[*websocket.Conn]bool = make(map[*websocket.Conn]bool)
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
@@ -15,10 +18,20 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("Failed to open connection:", err)
 	}
 
+	// save the new connected client
+	clients[conn] = true
+
+	// notifies all connected clients
+	for client := range clients {
+		client.Write(r.Context(), websocket.MessageText, []byte("New client connected"))
+	}
+
 	for {
 		_, data, err := conn.Read(r.Context())
 		if err != nil {
-			log.Println(err)
+			// conn.Write(r.Context(), websocket.MessageText, []byte("Client disconnected"))
+			log.Println("Client disconnected")
+			delete(clients, conn)
 			break
 		}
 
@@ -26,12 +39,20 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 		serverResponse := "Received: " + string(data)
 
-		conn.Write(r.Context(), websocket.MessageText, []byte(serverResponse))
+		for client := range clients {
+			client.Write(r.Context(), websocket.MessageText, []byte(serverResponse))
+		}
 	}
 }
 
+// clients connected
+func clientsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(strconv.Itoa(len(clients))))
+}
+
 func main() {
-	http.HandleFunc("/", wsHandler)
+	http.HandleFunc("/ws", wsHandler)
+	http.HandleFunc("/clients", clientsHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
