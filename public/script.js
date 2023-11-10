2f5840882b52f1875fed66cfaa52cb4abf39a6f3
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-11-10 00:34:20 -0300

    localhost dev setup

--- public/script.js
+++ public/script.js
@@ -53,7 +53,7 @@ createApp({
       }
 
       this.ws = new WebSocket(
-        `wss://go-websocket-production.up.railway.app/ws?nickname=${this.nickname}`,
+        `ws://localhost:3000/ws?nickname=${this.nickname}`,
       );
       this.ws.onopen = this.onOpen;
       this.ws.onmessage = this.onMessage;
@@ -70,9 +70,7 @@ createApp({
 
     async updateConnectedClients() {
       try {
-        const res = await fetch(
-          "https://go-websocket-production.up.railway.app/clients",
-        );
+        const res = await fetch("http://localhost:3000/clients");
         const data = await res.json();
         this.clients = data;
       } catch (error) {
